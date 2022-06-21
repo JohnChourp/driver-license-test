66d1bac5267d7e1eae97830bf670ea4305251e8a
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-06-21 14:45:22 +0300

    fixed url problem

---
+++ filesAndHrefIndex.js
@@ -0,0 +1,44 @@
+function addfilesIndex() {
+	let httpLink;
+	let hostName = window.location.host;
+
+	if (hostName === "johnchourp.github.io") {
+		httpLink = "https://";
+		hostName = window.location.host + "driver-license-test";
+
+	} else {
+		hostName = window.location.host;
+		httpLink = "http://";
+	}
+
+	let bodyCSS = document.createElement("link");
+	bodyCSS.rel = "stylesheet";
+	bodyCSS.type = "text/css";
+	bodyCSS.href = httpLink + hostName + "/CSS/body.css";
+
+	let testsCSS = document.createElement("link");
+	testsCSS.rel = "stylesheet";
+	testsCSS.type = "text/css";
+	testsCSS.href = httpLink + hostName + "/CSS/tests.css";
+
+	document.head.appendChild(bodyCSS);
+	document.head.appendChild(testsCSS);
+}
+addfilesIndex();
+
+function filesHrefIndex() {
+	let httpLink;
+	let hostName = window.location.host;
+
+	if (hostName === "johnchourp.github.io") {
+		httpLink = "https://";
+		hostName = window.location.host + "driver-license-test";
+
+	} else {
+		hostName = window.location.host;
+		httpLink = "http://";
+	}
+	let tests = document.getElementsByClassName("tests");
+	tests.item(0).href = httpLink + hostName + "/TaKuriaOrganaTouOximatosKaiTaXeiristiriaStoXoroOdigisis/test.html";
+}
+
